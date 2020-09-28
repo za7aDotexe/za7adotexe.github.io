505cce50248716505169c56707036fe391d640c7
za7aDotexe <rabiezaha@gmail.com>
2020-09-28 19:18:13 +0100

    work on progress ...

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('Its working')
+// console.log('Its working')
 
 let theme = localStorage.getItem('theme')
 
@@ -14,7 +14,7 @@ let themeDots = document.getElementsByClassName('theme-dot')
 for (var i=0; themeDots.length > i; i++){
 	themeDots[i].addEventListener('click', function(){
 		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
+		// console.log('Option clicked:', mode)
 		setTheme(mode)
 	})
 }
@@ -27,14 +27,5 @@ function setTheme(mode){
 	if(mode == 'blue'){
 		document.getElementById('theme-style').href = 'blue.css'
 	}
-
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
-
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
-
 	localStorage.setItem('theme', mode)
 }
