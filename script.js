9861b63d98e35c0237b36f57259073a7fb50d5b9
za7aDotexe <rabiezaha@gmail.com>
2020-10-02 02:11:08 -0100

    working on a preloader

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+const loader = document.querySelector('.loader');
+const main = document.querySelector('.main');
+
+function init() {
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    loader.style.display = 'none';
+
+    main.style.display = 'block';
+    setTimeout(() => (main.style.opacity = 1), 50);
+  }, 4000);
+}
+
+init();
