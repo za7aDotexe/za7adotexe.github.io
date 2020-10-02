382c35038045e1da98ab301a41ec07ab25446b75
za7aDotexe <rabiezaha@gmail.com>
2020-10-02 15:31:23 -0100

    fix the fuc*ing js

--- themeswitch.js
+++ themeswitch.js
@@ -2,21 +2,20 @@ let currentTheme = localStorage.getItem('theme');
 let toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'blue.css') {
+  if (currentTheme === 'blue.css') {
 		toggleSwitch.checked = true;
 		document.getElementById('theme-style').href = 'blue.css'
-    }
+  }
 }
 
 function switchTheme(e) {
     if (e.target.checked) {
-		document.getElementById('theme-style').href = 'blue.css'
-        localStorage.setItem('theme', 'blue.css');
+		  document.getElementById('theme-style').href = 'blue.css'
+      localStorage.setItem('theme', 'blue.css');
     }
     else {
-		document.getElementById('theme-style').href = 'default.css'
-        localStorage.setItem('theme', 'default.css');
+		  document.getElementById('theme-style').href = 'default.css'
+      localStorage.setItem('theme', 'default.css');
     }    
 }
 
