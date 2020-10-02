6dcd4e9ca87677288253904b79a79edfb89648d0
za7aDotexe <rabiezaha@gmail.com>
2020-10-02 14:53:38 -0100

    preloader woking !

---
+++ loader.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+  const main = document.querySelector(".main");
+  loader.classList.add("loader-finish");
+  main.classList.add("show-main");
+});
--- themeswitch.js
+++ themeswitch.js
@@ -1,7 +1,6 @@
 let currentTheme = localStorage.getItem('theme');
 let toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'blue.css') {
@@ -23,17 +22,3 @@ function switchTheme(e) {
 
 toggleSwitch.addEventListener('change', switchTheme, false);
 
-const loader = document.querySelector('.loader');
-const main = document.querySelector('.main');
-
-function init() {
-  setTimeout(() => {
-    loader.style.opacity = 0;
-    loader.style.display = 'none';
-
-    main.style.display = 'block';
-    setTimeout(() => (main.style.opacity = 1), 50);
-  }, 4000);
-}
-
-init();
